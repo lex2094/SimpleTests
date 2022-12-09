f2001aed8f10f6a51634679120f3f241a35b8395
lex2094 <alesha1240@mail.ru>
2022-12-10 03:59:23 +0800

    added allure steps

--- src/test/java/demoqa/RegistrationFormTest.java
+++ src/test/java/demoqa/RegistrationFormTest.java
@@ -8,38 +8,38 @@ import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-public class RegistrationFormTest {
-    @BeforeAll
-    static void configure() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-    }
+public class RegistrationFormTest extends TestBase {
 
     @Test
     void fillFormTest() {
+        step("Open form", () -> {
             open("/automation-practice-form");
             $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
             executeJavaScript("$('footer').remove()");
             executeJavaScript("$('#fixedban').remove()");
-            $("#firstName").setValue("Alex");
-            $("#lastName").setValue("Egorov");
-            $("#userEmail").setValue("alex@egorov.com");
-            $("#genterWrapper").$(byText("Other")).click(); // best
-            $("#userNumber").setValue("1234567890");
-            $("#dateOfBirthInput").click();
-            $(".react-datepicker__month-select").selectOption("July");
-            $(".react-datepicker__year-select").selectOption("2008");
-            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-            $("#subjectsInput").setValue("Math").pressEnter();
-            $("#hobbiesWrapper").$(byText("Sports")).click(); // best
-            $("#uploadPicture").uploadFromClasspath("img/1.png");
-            $("#currentAddress").setValue("Some address 1");
-            $("#state").click();
-            $("#stateCity-wrapper").$(byText("NCR")).click();
-            $("#city").click();
-            $("#stateCity-wrapper").$(byText("Delhi")).click();
-            $("#submit").click();
+            });
+        step("Fill form", () -> {
+                    $("#firstName").setValue("Alex");
+                    $("#lastName").setValue("Egorov");
+                    $("#userEmail").setValue("alex@egorov.com");
+                    $("#genterWrapper").$(byText("Other")).click(); // best
+                    $("#userNumber").setValue("1234567890");
+                    $("#dateOfBirthInput").click();
+                    $(".react-datepicker__month-select").selectOption("July");
+                    $(".react-datepicker__year-select").selectOption("2008");
+                    $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+                    $("#subjectsInput").setValue("Math").pressEnter();
+                    $("#hobbiesWrapper").$(byText("Sports")).click(); // best
+                    $("#uploadPicture").uploadFromClasspath("img/1.png");
+                    $("#currentAddress").setValue("Some address 1");
+                    $("#state").click();
+                    $("#stateCity-wrapper").$(byText("NCR")).click();
+                    $("#city").click();
+                    $("#stateCity-wrapper").$(byText("Delhi")).click();
+                    $("#submit").click();
+                });
             System.out.println("done");
     }
 }
--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -1,4 +1,12 @@
 package demoqa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
 public class TestBase {
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
 }
