950b62b0f17ad3a4b69d9ea316cb28df3e2c38e9
lex2094 <alesha1240@mail.ru>
2022-12-11 03:46:33 +0800

    added selenideLogger

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -7,15 +7,29 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URI;
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote ="https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "UNKNOWN");
+        capabilities.setCapability("browserVersion", "");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+      Configuration.browserCapabilities = capabilities;
+      Configuration.baseUrl = "https://demoqa.com";
+      Configuration.browserSize = "1920x1080";
+      Configuration.remote ="https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
 
     @AfterEach
